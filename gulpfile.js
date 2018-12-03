42ca6666ee8d5eceb82c308cab37be24f59e2861
lipanhong <1207006974@qq.com>
2018-12-03 09:44:45 +0800

    压缩js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");//编译scss
 var server = require("gulp-webserver");//启动服务
 var concat = require("gulp-concat")//合并
-
+var uglify = require("gulp-uglify");
 
 gulp.task("devSass",function(){
     return gulp.src("./src/scss/*.scss")
@@ -24,8 +24,11 @@ gulp.task("server",function(){
         }
     }))
 })
-
+//合并js
 gulp.task("concatJs",function(){
     return gulp.src(["./src/js/**/*.js","!./src/js/libs/*.js"])
-    .pipe(concat("./src/js/conjs"))
+    .pipe(concat("conjs"))
+    .pipe(uglify())//压缩js
+    .pipe(gulp.dest("./src/js/conjs"))
 })
+
---
+++ src/js/common.js
@@ -0,0 +1,6 @@
+var e  = 10;
+var f = 10;
+function c(){
+    console.log(e+f)
+}
+c()
