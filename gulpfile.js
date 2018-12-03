6aa448611ae607a93372ef69f109ce295ae91a89
lipanhong <1207006974@qq.com>
2018-12-03 09:28:22 +0800

    启动server

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");//编译scss
+var server = require("gulp-webserver");//启动服务
 
 gulp.task("devSass",function(){
     return gulp.src("./src/scss/*.scss")
@@ -8,4 +9,16 @@ gulp.task("devSass",function(){
 })
 gulp.task("watch",function(){
     return gulp.watch("./src/scss/*.scss",gulp.series("devSass"))
+})
+
+gulp.task("server",function(){
+    return gulp.src("./src")
+    .pipe(server({
+        port:9898,
+        open:true,
+        livereload:true,
+        middleware:function(req,res,next){
+
+        }
+    }))
 })
