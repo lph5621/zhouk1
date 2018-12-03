32ff6379da64b55b8bc715b9565c6b19cd174655
lipanhong <1207006974@qq.com>
2018-12-03 09:17:05 +0800

    编译scss

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");//编译scss
+
+gulp.task("devSass",function(){
+    return gulp.src("./src/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./src/css"))
+})
+gulp.task("watch",function(){
+    return gulp.watch("./src/scss/*.scss",gulp.series("devSass"))
+})
